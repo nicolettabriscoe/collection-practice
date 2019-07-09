9746cfb6629c7c30c2b3b374545d2e464ffc2489
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-09 15:21:18 -0500

    Second Commit

---
+++ Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+////declare a new empty array that stores strings
+//var friendsOfKarlie = [String]()
+//
+////write strings into my array
+//friendsOfKarlie = ["Michelle Obama","Serena Williams","T Swift","Jimmy Fallon"]
+//
+////there are 4 friends, or elements
+////each have an assigned location
+////Michelle Obama is at location 0, or index 0
+////Jimmy Fallon is index 3
+//print(friendsOfKarlie[0])  //should print Michelle Obama
+//print(friendsOfKarlie[2]) //should print T Swift
+//
+//friendsOfKarlie.append("Josh Kushner") //.append adds it to the end of the array
+////check if Josh is at index 4
+//print(friendsOfKarlie[4])
+//
+////replace Serena Williams with Megan
+//friendsOfKarlie[1] = "Megan" //assigns Serena's index to Megan
+//print (friendsOfKarlie[1])
+//
+////Want to remove Jimmy Fallon from friend list
+//friendsOfKarlie.remove(at: 3) //says to remove the element at index 3
+//print(friendsOfKarlie[3]) //prints Josh since we removed Jimmy, so now Josh is at index 3
+
+
+var roleModels = ["Karlie", "Nicoletta", "Maira"]
+//print second person
+//print(roleModels[1])
+////add kloss
+//roleModels.append("Kloss")
+////change nicoletta to serena
+//roleModels[1] = "Serena"
+////remove maira
+//roleModels.remove (at: 2)
+
+
+////I am declaring a dictionary
+//var recipe = ["almond flour": "3 and 1/2 cups","gluten-free oats": "2 cups", "mini chocolate chips" : "1 cup"]
+////key/value pairs
+////keys: almond flour, gluten free oats, and mini chocolate chips
+////values: 3 and 1/2 cups, 2 cups, and 1 cup
+//
+////how to declare dictionary without knowing data
+//var bread : [String : String] = [:]
+//print(bread)
+////adding key of flour and assigning it a value
+//bread["almond flour"] = "3 and 1/2 cups"
+//bread["gluten-free oats"] = "2 cups"
+//print(bread)
+//print(bread["almond flour"]!)
+//print(bread["gluten-free oats"]!) //adding ! gets rid of optional since we know there is info stored
+//print(Array(bread.keys)) //only prints keys
+//print(bread.values)     //only prints values
+//
+//
